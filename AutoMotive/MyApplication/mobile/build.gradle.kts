3f9a9d47ad49a33fdd6cc8ce5dfdb9dc6b1bbb83
ShubhamIEL <shubham.gawade@ielektron.com>
2025-07-28 19:45:01 +0530

    Any remain updates

--- AutoMotive/MyApplication/mobile/build.gradle.kts
+++ AutoMotive/MyApplication/mobile/build.gradle.kts
@@ -1,9 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("com.google.dagger.hilt.android")
-    // Make sure you have this for annotation processing
-    kotlin("kapt")
 }
 
 android {
@@ -12,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.myapplication"
-        minSdk = 29 // Using 29 as required by the car libraries
+        minSdk = 28
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -29,50 +26,24 @@ android {
             )
         }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
     kotlinOptions {
         jvmTarget = "11"
-    }
-
-    buildFeatures {
-        viewBinding = true
     }
 }
 
 dependencies {
-    // Core and UI Libraries
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-
-    // ViewModel and LiveData
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.lifecycle.livedata.ktx)
-
-    // Automotive Libraries
-    implementation(libs.androidx.app.automotive)
-    implementation("androidx.car.app:app:1.7.0")
-    implementation(libs.androidx.cardview) // Use the latest stable    compileOnly("com.google.android.automotive:automotive:1.2.0")
-
-    // Testing Libraries
-
+    implementation(project(":shared"))
+    testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    testImplementation(libs.junit)
-    testImplementation(libs.androidx.core.testing)
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
-
-    testImplementation("org.mockito.kotlin:mockito-kotlin:6.0.0")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
-
-    implementation("com.google.dagger:hilt-android:2.57")
-    kapt("com.google.dagger:hilt-android-compiler:2.57")
-
 }
