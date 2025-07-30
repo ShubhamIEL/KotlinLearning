370d27569381b8491b099b66b4f6e95ca98d57c4
ShubhamIEL <shubham.gawade@ielektron.com>
2025-07-30 16:55:40 +0530

    Delete MyApplication directory

--- MyApplication/core/src/main/java/com/example/myapplication/core/BindingAdapters.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.core
-
-import android.view.View
-import androidx.databinding.BindingAdapter
-
-@BindingAdapter("app:isVisible")
-fun setVisible(view: View, isVisible: Boolean) {
-    view.visibility = if (isVisible) View.VISIBLE else View.GONE
-}
