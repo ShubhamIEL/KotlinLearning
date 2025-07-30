493b495d37ec6dcc2fb5424e1abea9b2be0d49aa
ShubhamIEL <shubham.gawade@ielektron.com>
2025-07-30 11:43:54 +0530

    LiveData vs. StateFlow

--- MyApplication/feature_profile/src/main/java/com/example/myapplication/feature_profile/ProfileViewModel.kt
+++ MyApplication/feature_profile/src/main/java/com/example/myapplication/feature_profile/ProfileViewModel.kt
@@ -6,15 +6,20 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 import kotlinx.coroutines.flow.collect
 
 @HiltViewModel
-class ProfileViewModel @Inject constructor(private val repository: ProfileRepository
-) : ViewModel() {
-    private val _userName = MutableLiveData<String>()
-    val userName: LiveData<String> = _userName
+class ProfileViewModel @Inject constructor(private val repository: ProfileRepository ) : ViewModel() {
+
+    private  val _userName = MutableStateFlow("initial")
+    val userName : StateFlow<String> = _userName.asStateFlow()
+    //    private val _userName = MutableLiveData<String>()
+    //    val userName: LiveData<String> = _userName
 
     private val _welcomeMsg = MutableLiveData<String>()
     val welcomeMsg: LiveData<String> = _welcomeMsg
